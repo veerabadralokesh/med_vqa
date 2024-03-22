ad479850b6b24cc3930e3a182d5111f0af1ce1cd
mattragoza <mtr22@pitt.edu>
2024-03-22 13:41:20 -0400

    update test_llama script

--- test_llama.py
+++ test_llama.py
@@ -1,5 +1,5 @@
 # https://huggingface.co/docs/transformers/model_doc/llama
-import os; print(os.environ['HF_TOKEN']) 
+import sys, os; print(os.environ['HF_TOKEN']) 
 import torch
 import transformers as T
 
@@ -9,10 +9,11 @@ model_url = 'meta-llama/Llama-2-7b-hf'
 tokenizer = T.AutoTokenizer.from_pretrained(model_url)
 model = T.LlamaForCausalLM.from_pretrained(model_url).to(device)
 
-prompt = "Hey, are you conscious? Can you talk to me?"
+prompt, max_length = sys.argv[1:3]
+max_length = int(max_length)
 inputs = tokenizer.encode(prompt, return_tensors="pt").to(device)
 
-generate_ids = model.generate(inputs, max_length=30)
+generate_ids = model.generate(inputs, max_length=max_length)
 output = tokenizer.batch_decode(
     generate_ids,
     skip_special_tokens=True,
